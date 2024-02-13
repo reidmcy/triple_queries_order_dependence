a172bf349305dffdb111e1d4d44f04e237ec4daf
Katrina <katrinabrown@college.harvard.edu>
2024-02-13 14:24:02 -0500

    debug llama order dependence

--- modeling_llama_attention.py
+++ modeling_llama_attention.py
@@ -5,48 +5,208 @@ from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Un
 import types
 from transformers.generation import utils as generation_utils
 from transformers.utils import ModelOutput
-VERBOSE=False
+from torch.nn import CrossEntropyLoss
+from transformers.modeling_outputs import CausalLMOutputWithPast, BaseModelOutputWithPast
+from transformers.modeling_attn_mask_utils import _prepare_4d_causal_attention_mask, _prepare_4d_causal_attention_mask_for_sdpa
+from transformers.cache_utils import Cache, DynamicCache
+VERBOSE=True
 
-def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
-    """
-    Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
-    """
-    bsz, src_len = mask.shape[0],mask.shape[-1]
-    tgt_len = tgt_len if tgt_len is not None else src_len
-    if mask.shape==(bsz,1,tgt_len,src_len):
-        # If a 2D attention mask is passed in, use it as given
-        expanded_mask = mask.to(dtype)
-    else:
-        # If a 1D attention mask is passed in, expand it to the 2D attention mask following the default huggingface behavior
-        expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
+def forwardLlamaModel(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ):
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        use_cache = use_cache if use_cache is not None else self.config.use_cache
 
-    inverted_mask = 1.0 - expanded_mask
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-    return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
+        # retrieve input_ids and inputs_embeds
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        elif input_ids is not None:
+            batch_size, seq_length = input_ids.shape[:2]
+        elif inputs_embeds is not None:
+            batch_size, seq_length = inputs_embeds.shape[:2]
+        else:
+            raise ValueError("You have to specify either input_ids or inputs_embeds")
 
-def _prepare_decoder_attention_mask(self, attention_mask, input_shape, inputs_embeds, past_key_values_length):
-        # create causal mask
-        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-        combined_attention_mask = None
-        if input_shape[-1] > 1:
-            combined_attention_mask = _make_causal_mask(
-                input_shape,
-                inputs_embeds.dtype,
-                device=inputs_embeds.device,
-                past_key_values_length=past_key_values_length,
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                use_cache = False
+
+        past_key_values_length = 0
+        if use_cache:
+            use_legacy_cache = not isinstance(past_key_values, Cache)
+            if use_legacy_cache:
+                past_key_values = DynamicCache.from_legacy_cache(past_key_values)
+            past_key_values_length = past_key_values.get_usable_length(seq_length)
+
+        if position_ids is None:
+            device = input_ids.device if input_ids is not None else inputs_embeds.device
+            position_ids = torch.arange(
+                past_key_values_length, seq_length + past_key_values_length, dtype=torch.long, device=device
             )
+            position_ids = position_ids.unsqueeze(0)
 
-        if attention_mask is not None:
-            # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-            expanded_attn_mask = _expand_mask(attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1]).to(
-                inputs_embeds.device
+        if inputs_embeds is None:
+            inputs_embeds = self.embed_tokens(input_ids)
+
+        if self._use_flash_attention_2:
+            # 2d mask is passed through the layers
+            attention_mask = attention_mask if (attention_mask is not None and 0 in attention_mask) else None
+        elif self._use_sdpa and not output_attentions:
+            # output_attentions=True can not be supported when using SDPA, and we fall back on
+            # the manual implementation that requires a 4D causal mask in all cases.
+            attention_mask = _prepare_4d_causal_attention_mask_for_sdpa(
+                attention_mask,
+                (batch_size, seq_length),
+                inputs_embeds,
+                past_key_values_length,
             )
-            combined_attention_mask = (
-                expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
+        else:
+            # 4d mask is passed through the layers
+            attention_mask = _prepare_4d_causal_attention_mask(
+                attention_mask, (batch_size, seq_length), inputs_embeds, past_key_values_length
             )
-            # replace -inf values with torch.finfo(dtype).min in combined_attention_mask
-            combined_attention_mask = combined_attention_mask.masked_fill(combined_attention_mask.to(torch.bool), torch.finfo(inputs_embeds.dtype).min)
-        return combined_attention_mask
+            print(f"4D attention mask {attention_mask.shape}")
+            print(attention_mask)
+
+        # embed positions
+        hidden_states = inputs_embeds
+
+        # decoder layers
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attns = () if output_attentions else None
+        next_decoder_cache = None
+
+        for decoder_layer in self.layers:
+            if output_hidden_states:
+                all_hidden_states += (hidden_states,)
+
+            if self.gradient_checkpointing and self.training:
+                layer_outputs = self._gradient_checkpointing_func(
+                    decoder_layer.__call__,
+                    hidden_states,
+                    attention_mask,
+                    position_ids,
+                    past_key_values,
+                    output_attentions,
+                    use_cache,
+                )
+            else:
+                layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=attention_mask,
+                    position_ids=position_ids,
+                    past_key_value=past_key_values,
+                    output_attentions=output_attentions,
+                    use_cache=use_cache,
+                )
+
+            hidden_states = layer_outputs[0]
+
+            if use_cache:
+                next_decoder_cache = layer_outputs[2 if output_attentions else 1]
+
+            if output_attentions:
+                all_self_attns += (layer_outputs[1],)
+
+        hidden_states = self.norm(hidden_states)
+
+        # add hidden states from the last decoder layer
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        next_cache = None
+        if use_cache:
+            next_cache = next_decoder_cache.to_legacy_cache() if use_legacy_cache else next_decoder_cache
+        if not return_dict:
+            return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)
+        return BaseModelOutputWithPast(
+            last_hidden_state=hidden_states,
+            past_key_values=next_cache,
+            hidden_states=all_hidden_states,
+            attentions=all_self_attns,
+        )
+def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ):
+        # NOTE: print debugging info
+        print(f"Position IDs={position_ids}")
+        print(attention_mask)
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        hidden_states = outputs[0]
+        if self.config.pretraining_tp > 1:
+            lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
+            logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
+            logits = torch.cat(logits, dim=-1)
+        else:
+            logits = self.lm_head(hidden_states)
+        logits = logits.float()
+
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens
+            loss_fct = CrossEntropyLoss()
+            shift_logits = shift_logits.view(-1, self.config.vocab_size)
+            shift_labels = shift_labels.view(-1)
+            # Enable model parallelism
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss = loss_fct(shift_logits, shift_labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
 def _update_model_kwargs_for_generation(
         self,
@@ -73,19 +233,29 @@ def _update_model_kwargs_for_generation(
                 attention_mask = model_kwargs["attention_mask"]
                 if len(attention_mask.shape) == 4:
                     # given an attention_mask of shape (bsz, 1, tgt_seq_len, src_seq_len) in model kwargs, generate 
-                    # new 2D attention_mask of ones with shape (bsz, src_seq_len+1)
-                    # TODO: this ignores any padding tokens in the input_ids, which may be a problem?!?!
+                    # new 2D attention_mask of ones with shape (bsz, src_seq_len+1) for subsequent forward() calls
                     model_kwargs["attention_mask"] = torch.ones([attention_mask.shape[0], attention_mask.shape[-1]+1])
                     next_position_id = model_kwargs["position_ids"][0][-1]+1 if "position_ids" in model_kwargs else attention_mask.shape[-1]
                     model_kwargs["position_ids"] = torch.tensor([[next_position_id]])
+                    if VERBOSE:
+                        am=model_kwargs['attention_mask']
+                        print(f"2D attention mask shape={am.shape},{am.dtype}")
+                        print(attention_mask)
+                        print("After",am)
                 else:
                     # Extend the length of the attention mask by 1 to reflect the fact that we have generated one additional token
                     assert(len(attention_mask.shape) == 2)
                     model_kwargs["attention_mask"] = torch.cat(
                         [attention_mask, attention_mask.new_ones((attention_mask.shape[0], 1))], dim=-1
                     )
+                    
                     next_position_id = model_kwargs["position_ids"][0][-1]+1 if "position_ids" in model_kwargs else attention_mask.shape[-1]
                     model_kwargs["position_ids"] = torch.tensor([[next_position_id]])
+                    if VERBOSE:
+                        print(f"1D attention mask shape={model_kwargs['attention_mask'].shape}",model_kwargs['attention_mask'].dtype)
+            else:
+                if VERBOSE:
+                    print(f"attention_mask not in model_kwargs")
         else:
             # update decoder attention mask
             if "decoder_attention_mask" in model_kwargs:
@@ -98,7 +268,11 @@ def _update_model_kwargs_for_generation(
         return model_kwargs
 
 def get_2D_attention_accepting_model_llama(model):
-    model.model._prepare_decoder_attention_mask = types.MethodType(_prepare_decoder_attention_mask, model.model)
+    # Since llama2 already accepts 4D attention masks as input to forward(), we only need to override the _update_model_kwargs_for_generation() method to enable model.generate() to accept 4D attention masks as input as well.
     model._update_model_kwargs_for_generation = types.MethodType(_update_model_kwargs_for_generation, model)
+    
+    # NOTE: the following two methods are only overridden for the sake of printing out deubbing information. Their behavior is not actually modified. 
+    model.forward = types.MethodType(forward, model)
+    model.model.forward = types.MethodType(forwardLlamaModel, model.model)
     return model
 # modelLlama=get_attention_accepting_model(modelLlama)
